bb999f06ca36e2ff891de8f87b50ea5bf189a517
yasiruAtHippohotels <155042993+yasiruAtHippohotels@users.noreply.github.com>
2024-01-30 17:37:09 +0530

    creating homepage

--- ui-ux-website/components/Button.tsx
+++ ui-ux-website/components/Button.tsx
@@ -1,20 +1,21 @@
-import Image from 'next/image';
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
-type ButtonProps={
-    type:'button'|'submit';
-    title:string;
-    icon?:string;
-    variant:'btn_dark_green';
-}
+type ButtonProps = {
+  type: "button" | "submit";
+  title: string;
+  icon?: string;
+  variant: string;
+};
 
-
-
-export const Button = ({type,title,icon,variant} : ButtonProps) => {
+export const Button = ({ type, title, icon, variant }: ButtonProps) => {
   return (
-    <button type={type} className={`flexCenter gap-3 rounded-full border ${variant}`}>
-        {icon && <Image src={icon} alt={title} width={24} height={24}/>}
-        <label className='bold-16 whitespace-nowrap'>{title}</label>
+    <button
+      type={type}
+      className={`flexCenter gap-3 rounded-full border ${variant}`}
+    >
+      {icon && <Image src={icon} alt={title} width={24} height={24} />}
+      <label className="bold-16 whitespace-nowrap">{title}</label>
     </button>
-  )
-}
+  );
+};
--- ui-ux-website/components/Camp.tsx
+++ ui-ux-website/components/Camp.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+const CampSite = () => {
+  return <div>Camp Site 1</div>;
+};
 
 function Camp() {
   return (
-    <div>Camp</div>
-  )
+    <section className=" border-2 border-green-500 2x1:max-container relative flex flex-col py-10 lg:mb-10 lg:py-20 xl:mb-20">
+      <div className="hide-scrollbar flex h-[340px] w-full items-start justify-start gap-8 overflow-x-auto lg:h-[400px] xl:h-[640px]"></div>
+      <CampSite
+        backgroundImage="bg-bg-img-1"
+        title="Putuk Truno Camp"
+        subtitle="Prigen, Pasuruan"
+        peopleJoined="50+ Joined"
+      />
+      <CampSite />
+    </section>
+  );
 }
 
-export default Camp
+export default Camp;
