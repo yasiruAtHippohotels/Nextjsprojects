89cb31c0efebef37aa48c1ad6f1988b58e1ebb56
yasiruAtHippohotels <155042993+yasiruAtHippohotels@users.noreply.github.com>
2024-01-31 14:31:27 +0530

    add auth files

--- new-next-tutorial/src/app/_lib/page.tsx
+++ new-next-tutorial/src/app/_lib/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function PrivateRoute() {
-  return <h1>you cannot view this in the browser</h1>;
+  return <h1>you cannot view this in th browser</h1>;
 }
 
 export default PrivateRoute;
