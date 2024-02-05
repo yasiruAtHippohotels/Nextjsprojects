34d362ed75d31556b02f9b6a7122b97d7bc74e36
yasiru2077 <yasiruudana1999@gmail.com>
2024-02-05 07:51:15 +0530

    error handling

--- new-next-tutorial/src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ new-next-tutorial/src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,5 +1,11 @@
+"use client"
 import { notFound } from "next/navigation";
 import React from "react";
+
+
+function getRandomInt(count: number){
+  return Math.floor(Math.random() * count);
+}
 
 function ReviewDetail({
   params,
@@ -9,9 +15,16 @@ function ReviewDetail({
     reviewId: string;
   };
 }) {
+  const random = getRandomInt(2);
+
+  if(random === 1){
+    throw new Error("Error loading review");
+  }
+
   if (parseInt(params.reviewId) > 1000) {
     notFound();
   }
+  
   return (
     <h1>
       Review {params.reviewId} for product {params.productId}
