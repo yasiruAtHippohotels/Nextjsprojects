85bf7789001572a964b190a356f9f41764037f9a
yasiruAtHippohotels <155042993+yasiruAtHippohotels@users.noreply.github.com>
2024-02-05 17:36:54 +0530

    adding tutorial content to next project

---
+++ socialsphere/src/app/page.tsx
@@ -0,0 +1,6 @@
+import Image from "next/image";
+
+export default function Home() {
+  throw new Error(`Error in Home`);
+  return <div>Home Page</div>;
+}
---
+++ socialsphere/src/components/navbar/links/Links.tsx
@@ -0,0 +1,35 @@
+import Link from "next/link";
+import React from "react";
+
+function Links() {
+  const links = [
+    {
+      title: "Homepage",
+      path: "/",
+    },
+    {
+      title: "About",
+      path: "/about",
+    },
+    {
+      title: "Contact",
+      path: "/contact",
+    },
+    {
+      title: "Blog",
+      path: "/blog",
+    },
+  ];
+
+  return (
+    <div className="flex flex-row gap-10">
+      {links.map((link) => (
+        <Link href={link.path} key={link.title}>
+          {link.title}
+        </Link>
+      ))}
+    </div>
+  );
+}
+
+export default Links;
